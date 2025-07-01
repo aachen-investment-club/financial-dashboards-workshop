770ba7951516f432ca7f5a3e0e80e83294a950a6
BenjaminOyarzun17 <benjaoyama@gmail.com>
2025-07-01 21:46:32 +0200

    plotly test

--- pages/Team 1.py
+++ pages/Team 1.py
@@ -1,9 +1,39 @@
 import streamlit as st
+import plotly.express as px
+import numpy as np
+import pandas as pd
 
-st.set_page_config(page_title=PICK_YOUR_NAME)
 
-st.title(PICK_A_TITLE)
+st.set_page_config(page_title="Team 1's subpage")
+
+st.title("Welcome to team 1's subpage!")
+
+
 
 st.write("""
 This is team 1's subpage!
 """)
+
+
+np.random.seed(42)
+df = pd.DataFrame({
+    'x': np.arange(1, 11),
+    'y': np.linspace(1,1000,10)
+})
+
+fig = px.line(
+    df, 
+    x='x', 
+    y='y', 
+    title='AIC portfolio value',
+    markers=True
+)
+
+fig.update_layout(
+    xaxis_title='Time',
+    yaxis_title='euro',
+)
+
+st.title("📈 Dummy Plotly Chart Example")
+
+st.plotly_chart(fig, use_container_width=True)
