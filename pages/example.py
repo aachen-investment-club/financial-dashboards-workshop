6836e9ae94f3eddbd1f8567281a8cb59a349fe9e
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2025-07-20 09:18:25 +0200

    cleaned main

--- pages/example.py
+++ pages/example.py
@@ -3,13 +3,3 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 
-
-NAME= "value2"
-
-st.set_page_config(page_title=NAME)
-
-st.title(NAME)
-
-st.write(f"""
-This to {NAME}'s project!
-""")
