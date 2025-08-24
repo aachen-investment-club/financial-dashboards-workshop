927afec3e0e0bc8ef8261251e71cb10cb8c7e489
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2025-08-24 12:26:53 +0200

    improved the homepage

--- homepage.py
+++ homepage.py
@@ -7,11 +7,27 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("Home")
+st.title("AIC - Financial Dashboards")
 st.write("""
-Welcome to the Dahsboards Workshop!
-- checkout the guide in ...
-""")
+        Welcome to AIC's financial dashboads!
+
+        In this website, we contain the results of the financial dashboards workshop.     
+         
+        We will merge your dashboards into this website. 
+        """)
+
+base_project_url = "/base-project-demo"
+repo_url = "https://github.com/aachen-investment-club/financial-dashboards-workshop"
+aic_url = "https://www.aachen-investment-club.de/"
+
+
+st.markdown(f"Checkout the base project: [base project](%s)" % base_project_url)
+
+st.markdown(f"Checkout the GitHub repository: [repo](%s)" % repo_url)
+
+
+st.markdown(f"Checkout our Website: [our website](%s)" % aic_url)
+
 
 # preload datasets in cache
 _ = load_data(name='sp500_close')
